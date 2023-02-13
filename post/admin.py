02cc01145961922827e381d44d2219ceff914059
bscoderuz <mrbscoder@gmail.com>
2023-02-13 05:40:59 -0800

    add templates folder

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from models import Post
+
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title', 'author')
+
+
+admin.site.regsiter(Post, PostAdmin)
